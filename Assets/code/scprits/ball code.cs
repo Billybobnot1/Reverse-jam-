7f50c2ac61a6906d80064ab39f97929adeb0ce52
Billybobnot1 <BILLYBOBNOT1@GMAIL.COM>
2025-01-20 17:43:20 -0800

    ball added

--- Assets/code/scprits/ball code.cs
+++ Assets/code/scprits/ball code.cs
@@ -1,6 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class NewEmptyCSharpScript
-{
-    
+public class Ball : MonoBehaviour { 
+
+// [Header ("References")] 
+public Rigidbody2D rb; 
+public LineRenderer lr;
+
+// [Header ("Attributes")] 
+public float maxPower = 10f; 
+public float power = 2f; 
+public float maxGoalSpeed = 4f; 
+
+private bool isDragging; 
+private bool inHole; 
+
+private void Update() { 
+    PlayerInput(); 
+
 }
+
+private void PlayerInput () { 
+    Vector2 inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+    float distance = Vector2.Distance(transform.position, inputPos);
+
+    if (Input.GetMouseButtonDown(0) && distance <=0.5f) DragStart();
+    if (Input.GetMouseButton(0) && isDragging) DragChange(); 
+    if (Input.GetMouseButtonUp(0) && isDragging) DragRelease(inputPos); 
+ 
+}
+
+public void DragStart () { 
+isDragging = true; 
+}
+private void DragChange() {} 
+private void DragRelease(Vector2 pos) {
+    float distance = Vector2.Distance((Vector2)transform.position, pos);
+    isDragging = false; 
+
+    if (distance < 1f) { 
+        return; 
+    }
+    Vector2 dir = (Vector2)transform.position - pos; 
+
+    rb.linearVelocity = Vector2.ClampMagnitude(dir * power, maxPower);
+}
+}
+
